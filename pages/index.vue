243d99ee290e029b1877145185e2179974fc72bd
sxd <1447707563@qq.com>
2018-01-26 18:45:16 +0800

    排版完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,44 @@
 <template>
     <div>
     	<my-header></my-header>
-        <b-container>
-            <b-row>
-                <b-col cols="9">我占九个</b-col>
-                <b-col cols="3">我占三个</b-col>
-            </b-row>
-        </b-container>
+        <div class="index my-container">
+            <div class="row">
+                <div class="col-8 main">
+                    <b-carousel id="carousel1" controls indicators :interval="2500" v-model="slide">
+                        <b-carousel-slide>
+                            <img slot="img" width="100%" src="../assets/img/swiper-1.jpg" alt="image slot">
+                        </b-carousel-slide>
+                        <b-carousel-slide>
+                            <img slot="img" width="100%" src="../assets/img/swiper-2.jpg" alt="image slot">
+                        </b-carousel-slide>
+                        <b-carousel-slide>
+                            <img slot="img" width="100%" src="../assets/img/swiper-3.jpg" alt="image slot">
+                        </b-carousel-slide>
+                        <b-carousel-slide>
+                            <img slot="img" width="100%" src="../assets/img/swiper-4.jpg" alt="image slot">
+                        </b-carousel-slide>
+                        <b-carousel-slide>
+                            <img slot="img" width="100%" src="../assets/img/swiper-5.jpg" alt="image slot">
+                        </b-carousel-slide>
+                        <b-carousel-slide>
+                            <img slot="img" width="100%" src="../assets/img/swiper-6.jpg" alt="image slot">
+                        </b-carousel-slide>
+                        <b-carousel-slide>
+                            <img slot="img" width="100%" src="../assets/img/swiper-7.jpg" alt="image slot">
+                        </b-carousel-slide>
+                    </b-carousel>
+                </div>
+                <div class="col-4 aside">
+                    <div class="board">
+                        <img src="../assets/img/biaoqian-1.png" alt="">
+                        <img src="../assets/img/biaoqian-2.png" alt="">
+                        <img src="../assets/img/biaoqian-3.png" alt="">
+                        <img src="../assets/img/biaoqian-4.png" alt="">
+                        <img src="../assets/img/biaoqian-5.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
